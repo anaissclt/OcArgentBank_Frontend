0ac3c9bdc297493daf3ae9ef8d0f39917b0e6694
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-04-16 17:18:16 +0200

    Ajout de l'authentification sur la page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,36 @@
 // IMPORT
-import React from "react";
+import React, { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 // COMPOSANTS
 import User from "../../components/User/User";
 import Accounts from "../../components/Accounts/Accounts";
+import { logInWithToken } from "../../features/authSlice";
 
 function Profile() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn); // Hook useSelector pour l'état d'authentification
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Vérifie si l'option "Remember Me" est activée
+    const rememberMe = localStorage.getItem("token") !== null;
+
+    // Si l'utilisateur n'est pas connecté mais "Remember Me" est activé
+    if (!isLoggedIn && rememberMe) {
+      // Connecte l'utilisateur en utilisant le token du localStorage
+      dispatch(logInWithToken(localStorage.getItem("token")));
+      // Si l'utilisateur n'est pas connecté et "Remember Me" n'est pas activé
+    } else if (!isLoggedIn && !rememberMe) {
+      // redirige vers la page de connexion
+      window.location.replace("/login");
+    }
+  }, [isLoggedIn, dispatch]); // se déclenche lorsque isLoggedIn ou dispatch changent
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <>
       <main className="main bg-dark">
