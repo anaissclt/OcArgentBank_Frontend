f33185726594693c444e9f00c0568649115bb486
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-04-24 17:09:25 +0200

    Ajout de la modif user

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -29,6 +29,33 @@ export const fetchUserProfile = createAsyncThunk(
   }
 );
 
+export const updateUserName = createAsyncThunk(
+  'profile/updateUserName',
+  async (newUserName, thunkAPI) => {
+    try {
+      const token = sessionStorage.getItem("token") || localStorage.getItem("token");
+      const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({ userName: newUserName }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Échec maj user name');
+      }
+
+      const data = await response.json();
+      return data.body;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message || 'Échec maj user name');
+    }
+  }
+);
+
 const profileSlice = createSlice({
     name: "profile",
     initialState: {
@@ -53,6 +80,12 @@ const profileSlice = createSlice({
         })
         .addCase(fetchUserProfile.rejected, (state, action) => {
           state.error = action.payload;
+        })
+        .addCase(updateUserName.fulfilled, (state, action) => {
+          state.userName = action.payload.userName;
+        })
+        .addCase(updateUserName.rejected, (state, action) => {
+          state.error = action.payload.message;
         });
     },
   });
